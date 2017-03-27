646a2e56e8694095cdb097a3161faeeeff0f3a2f
“Zhu Chen” <znchen720@gmail.com>
2017-03-27 04:02:01 -0400

    Change Show Action in Weather Controller
    Changed the show action so that the system can accept both
    zip codes and strings as arguments for location when checking
    weather.

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -5,9 +5,16 @@ class WeathersController < ApplicationController
   # GET /weathers/1
   def show
     location = params[:id]
-    api_key = ENV['API_KEY']
-    uri = URI("http://api.openweathermap.org/data/2.5/weather?q=#{location},us&appid=#{api_key}")
-    @weather = Net::HTTP.get(uri)
-    render json: @weather
+    if location.is_a? Integer
+      api_key = ENV['API_KEY']
+      uri = URI("http://api.openweathermap.org/data/2.5/weather?zip=#{location},us&appid=#{api_key}")
+      @weather = Net::HTTP.get(uri)
+      render json: @weather
+    elsif location.is_a? String
+      api_key = ENV['API_KEY']
+      uri = URI("http://api.openweathermap.org/data/2.5/weather?q=#{location},us&appid=#{api_key}")
+      @weather = Net::HTTP.get(uri)
+      render json: @weather
+    end
   end
 end
